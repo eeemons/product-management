42edf910d55bc6ef005ee256b6c59777914c0c33
eeemons <eeemons@live.com>
2025-10-18 00:03:31 +0600

    added animation for the view switch

--- src/components/products/ProductsView.tsx
+++ src/components/products/ProductsView.tsx
@@ -6,6 +6,7 @@ import ProductCard from "./ProductCard";
 import ProductListItem from "./ProductListItem";
 import ViewToggle from "./ViewToggle";
 import Pagination from "./Pagination";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface ProductsViewProps {
   products: Product[];
@@ -22,26 +23,40 @@ const ProductsView = ({ products }: ProductsViewProps) => {
 
   const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
   return (
     <div className="flex flex-col">
       <div className="flex justify-end mb-4">
         <ViewToggle view={view} setView={setView} />
       </div>
-      <div>
-        {view === "grid" ? (
-          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
-            {currentProducts.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        ) : (
-          <div className="flex flex-col gap-4">
-            {currentProducts.map((product) => (
-              <ProductListItem key={product.id} product={product} />
-            ))}
-          </div>
-        )}
-      </div>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={view}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={variants}
+          transition={{ duration: 0.5 }}
+        >
+          {view === "grid" ? (
+            <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+              {currentProducts.map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col gap-4">
+              {currentProducts.map((product) => (
+                <ProductListItem key={product.id} product={product} />
+              ))}
+            </div>
+          )}
+        </motion.div>
+      </AnimatePresence>
       <div className="mt-auto pt-4">
         <Pagination
           productsPerPage={productsPerPage}
